5db6a0c78dfe57f3ca7a055722d1c4eecfce2148
Pedro <plazari96@gmail.com>
2016-12-05 19:09:54 -0200

    Release

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -46,7 +46,7 @@
                     <ul class="nav navbar-nav">
                         &nbsp;<li><a href="{{ url('send') }}">Enviar uma frase</a></li>
                         &nbsp;<li><a href="{{ url('frases') }}">Frases</a></li>
-                        &nbsp;<li><a href="{{ url('minhas-frases') }}"><span class="label label-success">NEW</span>Minhas Frases</a></li>
+                        &nbsp;<li><a href="{{ url('minhas-frases') }}">Minhas Frases<span class="label label-success">NEW</span></a></li>
                         &nbsp;<li><a href="#">Frases no Lixo<span class="label label-danger">Em breve</span></a></li>
                     </ul>
 
